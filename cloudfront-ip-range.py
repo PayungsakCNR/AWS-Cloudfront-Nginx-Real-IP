7445752b5279589b19a8f6994336d74e5e029479
PayungsakCNR <payungsakpk@payungsakpk.xyz>
2020-06-02 03:14:59 +0700

    Add try .. except
    Add try .. except

--- cloudfront-ip-range.py
+++ cloudfront-ip-range.py
@@ -8,22 +8,28 @@
 
 import requests, json
 
-d = requests.get('https://ip-ranges.amazonaws.com/ip-ranges.json').text
-l = json.loads(d)
+try:
+    d = requests.get('https://ip-ranges.amazonaws.com/ip-ranges.json').text
+    l = json.loads(d)
 
-f = open("cloudfront-ip-list-real-ip.conf","w+")
+    f = open("cloudfront-ip-list-real-ip.conf","w+")
 
-for ip_range in [x['ip_prefix'] for x in l['prefixes'] if x['service'] =='CLOUDFRONT' ]:
-        f.write("set_real_ip_from " + ip_range + ";" + "\r\n")
-### Private IP ###
-f.write ("set_real_ip_from 10.0.0.0/8;\r\n")
-f.write ("set_real_ip_from 172.16.0.0/12;\r\n")
-f.write ("set_real_ip_from 192.168.0.0/16;\r\n")
+    for ip_range in [x['ip_prefix'] for x in l['prefixes'] if x['service'] =='CLOUDFRONT' ]:
+            f.write("set_real_ip_from " + ip_range + ";" + "\r\n")
+    ### Private IP ###
+    f.write ("set_real_ip_from 10.0.0.0/8;\r\n")
+    f.write ("set_real_ip_from 172.16.0.0/12;\r\n")
+    f.write ("set_real_ip_from 192.168.0.0/16;\r\n")
 
-### Set Real IP from X-Forwarded-For HTTP Header ###
-f.write ("real_ip_header X-Forwarded-For;\r\n")
-### Enable REAL IP Recursive ###
-f.write ("real_ip_recursive on;")
+    ### Set Real IP from X-Forwarded-For HTTP Header ###
+    f.write ("real_ip_header X-Forwarded-For;\r\n")
+    ### Enable REAL IP Recursive ###
+    f.write ("real_ip_recursive on;")
 
-print ("Generate NGINX CONFIG FILE SUCCESSFULLY !!")
+    print ("Generate NGINX CONFIG FILE SUCCESSFULLY !!")
+except Exception as e:
+    print ("Error")
+    print (e)
+finally:
+    f.close()
 ### END ###
